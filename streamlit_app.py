8ba4f9896c0bfc80dc5aade0b4c31956561f88e7
Azaelf <128338632+azaelff@users.noreply.github.com>
2025-12-01 16:01:02 -0500

    CO NAN

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,5 @@ else:
 show_conan = st.sidebar.checkbox("Show Conan Gray Top Albums")
 
 if show_conan:
-    url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=conan_gray&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
+    url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Conan Gray&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
     st.write(requests.get(url).json())
