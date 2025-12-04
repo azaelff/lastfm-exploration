2b773a461e109b770849b700a511ca8ac3c8676b
Azaelf <128338632+azaelff@users.noreply.github.com>
2025-12-04 19:02:36 +0000

    HELP

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,18 @@ if choose_artist:
     url = f"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist={choose_artist}&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
     # requests.get(url).json()["toptags"]["tag"]
 
+    times = {}
+
     for tag in requests.get(url).json()["toptags"]["tag"]:
         tagname = tag["name"]
-        url = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={tagname}&limit=3&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
-        st.write(requests.get(url).json())
+        urltwo = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={tagname}&limit=3&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
+        artists = requests.get(urltwo).json()["topartists"]["artist"]
+        for artist in artists:
+            if artist["name"] not in times:
+               times[artist["name"]] = 1
+            else:
+               times[artist["name"]] += 1
+    
+    st.write(times)
+    st.write(requests.get(urltwo).json()["toptags"]["tag"])
 
