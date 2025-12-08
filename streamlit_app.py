7e68217652e6fc3b1aed02e689aa5103a62af1dd
Azaelf <128338632+azaelff@users.noreply.github.com>
2025-12-08 21:58:38 +0000

    code push #2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
+import numpy as np
 
 st.title("Explore The last.fm API")
 if "token" in st.query_params:
@@ -28,25 +29,26 @@ if choose_artist:
     url = f"http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist={choose_artist}&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
     tags = requests.get(url).json()["toptags"]["tag"]
     countweight = {}
-    for name in tags:
+    for tag in tags:
         countweight[tag["name"]] = (100-tag["count"])/10
 
-    times = {}
+    data = pd.DataFrame([], columns = ["tags", "artist", "value"])
 
     for tag in requests.get(url).json()["toptags"]["tag"]:
         tagname = tag["name"]
         urltwo = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={tagname}&limit=3&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
         artists = requests.get(urltwo).json()["topartists"]["artist"]
         for artist in artists:
-            if artist["name"] not in times:
-               times[artist["name"]] = 1
+            existingRow = data[data["artist"]==artist["name"]]
+            value = 10/19*(20 - (countweight[tagname] + int(artist["@attr"]["rank"])))
+            if existingRow.empty:
+                data.loc[-1] = [[tagname], artist["name"], value]
+                data.index += 1
             else:
-               times[artist["name"]] += 1
-
-
-
+                data.at[existingRow.index[0], "tags"].append(tagname)
+                data.at[existingRow.index[0], "value"] += value
 
     
-    st.write(times)
-    st.write(requests.get(url).json()["toptags"]["tag"])
+
+    st.write(data.sort_values("value", ascending=False))
 
