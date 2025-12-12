0be333a62930e8c203ab231719f3e5eb6d751faa
mhyun777 <mhyun@haverford.edu>
2025-12-11 23:14:56 -0500

    continuous niche

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import numpy as np
 
 st.title("[L]ast.fm Artist Recommendation Algorithm")
 if "token" in st.query_params:
-    st.write("API Enabled")
+    st.sidebar.write("API Enabled")
 else:
-    st.write(f'''
+    st.sidebar.write(f'''
          <a href="http://www.last.fm/api/auth/?api_key=68ec0071f9e7750afbd8f8f53d9659e0">
             <button>
                 Enable API
@@ -18,20 +18,11 @@ else:
         ''',
         unsafe_allow_html=True
     )
-niche = st.sidebar.select_slider("How niche would you like your recommendations?", ["Not Niche", "Sort of Niche", "Niche", "Very Niche"])
-st.sidebar.write(f"Showing {niche} artists")
-if niche == "Not Niche":
-    lim = 3
-    filter = 0
-if niche == "Sort of Niche":
-    lim = 10
-    filter = 5
-if niche == "Niche":
-    lim = 25
-    filter = 20
-if niche == "Very Niche":
-    lim = 50
-    filter = 40
+
+st.sidebar.write("How niche would you like your recommendations?")
+minNiche = st.sidebar.slider("Minimum artist ranking:", 1, 999, 1)
+maxNiche = st.sidebar.slider("Maximum artist ranking:", minNiche, 1000, 1000)
+st.sidebar.write(f"Showing artists ranked between #{minNiche} and #{maxNiche}")
 
 show_conan = st.sidebar.checkbox("Show Conan Gray Top Tags")
 if show_conan:
@@ -51,32 +42,28 @@ if choose_artist:
 
     for tag in requests.get(url).json()["toptags"]["tag"]:
         tagname = tag["name"]
-        urltwo = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={tagname}&limit={lim}&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
+        urltwo = f"http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag={tagname}&limit=1000&api_key=68ec0071f9e7750afbd8f8f53d9659e0&format=json"
         artists = requests.get(urltwo).json()["topartists"]["artist"]
-        for artist in artists:
-            condition = int(artist["@attr"]["rank"]) >= filter
-            if condition == True:
-                existingRow = data[data["artist"]==artist["name"]]
-                value = countweight[tagname]/10 + 100/(10+int(artist["@attr"]["rank"]))
-                if existingRow.empty:
-                    data.loc[-1] = [artist["name"], [tagname], value]
-                    data.index += 1
-                else:
-                    data.at[existingRow.index[0], "tags"].append(tagname)
-                    data.at[existingRow.index[0], "value"] += value
-
-
+        for artist in artists[minNiche: maxNiche]:
+            existingRow = data[data["artist"]==artist["name"]]
+            value = countweight[tagname]/10 + 100/(10+int(artist["@attr"]["rank"]))
+            if existingRow.empty:
+                data.loc[-1] = [artist["name"], [tagname], value]
+                data.index += 1
+            else:
+                data.at[existingRow.index[0], "tags"].append(tagname)
+                data.at[existingRow.index[0], "value"] += value
     
     df_final = data.sort_values("value", ascending=False).reset_index(drop=True)
     df_final.index += 1
     st.write(df_final)
 
-st.write(f'''
-         <a href="https://forms.gle/ovHbXLhv2TcNYe2D6">
-            <button>
-                After testing the algorithm, Give us your thoughts!
-            </button>
-         </a>
-        ''',
-        unsafe_allow_html=True
-    )
+    st.write(f'''
+            <a href="https://forms.gle/ovHbXLhv2TcNYe2D6">
+                <button>
+                    After testing the algorithm, Give us your thoughts!
+                </button>
+            </a>
+            ''',
+            unsafe_allow_html=True
+        )
